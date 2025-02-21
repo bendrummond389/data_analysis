bcae0c5e468a95ec395c7147587093735f8361a0
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2025-02-21 09:51:52 -0600

    random forest

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from .fips import Fips
 from .acs2017_county_data import ACS2017CountyData
 from .mortality_rate_2014 import MortalityRate2014
 from .car_price_dataset import CarPriceDataset
+from .netflix_dataset import NetflixDataset
 
 __all__ = [
     "Base",
@@ -10,4 +11,5 @@ __all__ = [
     "ACS2017CountyData",
     "MortalityRate2014",
     "CarPriceDataset",
+    "NetflixDataset",
 ]
---
+++ models/netflix_dataset.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, BigInteger, Text
+from sqlalchemy.ext.declarative import declarative_base
+from models.base import Base
+
+class NetflixDataset(Base):
+    __tablename__ = 'netflix_dataset'
+    
+    show_id = Column(String, primary_key=True)
+    type = Column(String)
+    title = Column(String)
+    director = Column(Text)  
+    cast = Column(Text)  
+    country = Column(Text)
+    date_added = Column(String)  
+    release_year = Column(BigInteger)
+    rating = Column(String)
+    duration = Column(String)  
+    listed_in = Column(Text)  
+    description = Column(Text)
+    text = Column(Text) 
