8d2348ee1b8c17328a508220230e0d54f25c6eda
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2025-02-22 15:30:40 -0600

    cleaned utils

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,6 +1,6 @@
-from data_cleaning import clean_data
-from utils import load_config, setup_logging
-from database import get_session, load_data_to_db
+from .data_cleaning import clean_data
+from .utils import load_config, setup_logging
+from .database import get_session, load_data_to_db
 
 
 __all__ = [
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,28 +1,23 @@
-import os
+from pathlib import Path
 import logging
 import yaml
 
-
-def get_project_root():
-    """Returns the absolute path to the project root directory."""
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-
 def load_config():
     """Loads the configuration file from the project root."""
-    config_path = os.path.join(get_project_root(), "config/config.yaml")
-    with open(config_path, "r") as file:
+    config_path = Path(__file__).resolve().parent.parent / "config/config.yaml"
+    with config_path.open("r") as file:
         return yaml.safe_load(file)
-
 
 def setup_logging(log_path):
     """Sets up logging with the specified log file path."""
-    logging.basicConfig(
-        filename=log_path,
-        level=logging.INFO,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-    )
-    # Optionally also log to console
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logging.getLogger("").addHandler(console)
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    
+    # Configure file logging
+    logging.basicConfig(filename=log_path, level=logging.INFO, format=log_format)
+    
+    # Configure console logging
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter(log_format))
+    
+    logging.getLogger().addHandler(console_handler)
