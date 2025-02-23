cf9457168d18a58bc5fcf0260e5d924cc6a1dca3
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2025-02-23 07:36:25 -0600

    more refactoring

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,15 +1,6 @@
 from .base import Base
-from .example_data.fips import Fips
-from .example_data.acs_2017 import ACS2017CountyData
-from .example_data.mortality_rates import MortalityRate2014
-from .example_data.car_prices import CarPriceDataset
-from .example_data.netflix_catalog import NetflixDataset
+
 
 __all__ = [
     "Base",
-    "Fips",
-    "ACS2017CountyData",
-    "MortalityRate2014",
-    "CarPriceDataset",
-    "NetflixDataset",
 ]
